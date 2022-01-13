b16c24198bbb28b07b991b56a5e2ed74e59d3466
mugman174 <redacted@redac.ted>
2022-01-13 19:25:37 +0000

    Initial Commit

---
+++ app.py
@@ -0,0 +1,5 @@
+from pick import pick
+
+guide = "https://github.com/Delitefully/DiscordLists/blob/master/flags.md"
+
+options = {"STAFF": 1, "PARTNER": 2, "HYPESQUAD": 4, "BUG_HUNTER_LEVEL_1": 8, "HYPESQUAD_ONLINE_HOUSE_1": 64, "HYPESQUAD_ONLINE_HOUSE_2": 128}
